a5693e6f6ee1f72c7c2d5d43d1d4f6f25bd237a5
jhayakrishnaKS <jhayakrishna.sainathbabu@kumaran.com>
2024-03-11 17:06:59 +0530

    done

--- main.go
+++ main.go
@@ -3,110 +3,82 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"log"
 	"net/http"
-	"strconv"
-	"github.com/gorilla/mux"
 )
 
 type Todo struct {
-	ID        string `json:"id"`
+	ID        uint   `json:"id"`
 	Title     string `json:"title"`
 	Completed bool   `json:"completed"`
 }
 
-var todos []Todo
+var db *gorm.DB
 
-func getTodos(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	if len(todos) == 0 {
-		message := map[string]string{"message": "No todos available"}
-		err := json.NewEncoder(w).Encode(message)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		return
+func initDB() {
+	dsn := "root:root@tcp(127.0.0.1:3306)/todo_api_go?charset=utf8mb4&parseTime=True&loc=Local"
+	var err error
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error connecting to database:", err)
 	}
+	db.AutoMigrate(&Todo{})
+}
+
+func GetTodos(w http.ResponseWriter, r *http.Request) {
+	var todos []Todo
+	db.Find(&todos)
 	json.NewEncoder(w).Encode(todos)
 }
 
-func getTodoById(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+func GetTodoById(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	for _, v := range todos {
-		if v.ID == params["id"] {
-			json.NewEncoder(w).Encode(v)
-			return
-		}
-	}
-	message := map[string]string{"message": "Todo not found"}
-	w.WriteHeader(http.StatusNotFound)
-	json.NewEncoder(w).Encode(message)
-}
-
-func deleteTodoById(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-Type", "application/json")
-	params := mux.Vars(r)
-	for i, v := range todos {
-		if v.ID == params["id"] {
-			todos = append(todos[:i], todos[i+1:]...)
-			message := map[string]string{"message": "todo has been deleted successfully"}
-			json.NewEncoder(w).Encode(message)
-			break
-		}
-		json.NewEncoder(w).Encode(todos)
-	}
-}
-
-
-var idCounter = 3
-
-func CreateTodo(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
 	var todo Todo
-	err := json.NewDecoder(r.Body).Decode(&todo)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	todo.ID = strconv.Itoa(idCounter)
-	idCounter++
-	todos = append(todos, todo)
+	db.First(&todo, params["id"])
 	json.NewEncoder(w).Encode(todo)
 }
 
-func editTodoById(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for i, v := range todos {
-		if v.ID == params["id"] {
-			todos = append(todos[:i], todos[i+1:]...)
+func CreateTodo(w http.ResponseWriter, r *http.Request) {
+	var todo Todo
+	json.NewDecoder(r.Body).Decode(&todo)
+	db.Create(&todo)
+	json.NewEncoder(w).Encode(todo)
+	json.NewEncoder(w).Encode("Todo has been created successfully")
 
-			var todo Todo
-			err := json.NewDecoder(r.Body).Decode(&todo)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			todo.ID = params["id"]
-			todos = append(todos, todo)
-			json.NewEncoder(w).Encode(todo)
-		}
-	}
+}
+
+func UpdateTodoById(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var todo Todo
+	db.First(&todo, params["id"])
+	json.NewDecoder(r.Body).Decode(&todo)
+	db.Save(&todo)
+	json.NewEncoder(w).Encode(todo)
+	json.NewEncoder(w).Encode("Todo has been updated successfully")
+
+}
+
+func DeleteTodoById(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var todo Todo
+	db.First(&todo, params["id"])
+	db.Delete(&todo)
+	json.NewEncoder(w).Encode("Todo deleted successfully")
 }
 
 func main() {
+	initDB()
+	defer db.Statement.ReflectValue.Close()
 	r := mux.NewRouter()
+	r.HandleFunc("/todos", GetTodos).Methods("GET")
+	r.HandleFunc("/todos/{id}", GetTodoById).Methods("GET")
+	r.HandleFunc("/todos", CreateTodo).Methods("POST")
+	r.HandleFunc("/todos/{id}", UpdateTodoById).Methods("PUT")
+	r.HandleFunc("/todos/{id}", DeleteTodoById).Methods("DELETE")
 
-	todos = append(todos, Todo{"1", "eat", false})
-	todos = append(todos, Todo{"2", "sleep", true})
-
-	r.HandleFunc("/todos", getTodos).Methods("GET")
-	r.HandleFunc("/todo/{id}", getTodoById).Methods("GET")
-	r.HandleFunc("/todo/create", CreateTodo).Methods("POST")
-	r.HandleFunc("/todo/edit/{id}", editTodoById).Methods("PUT")
-	r.HandleFunc("/todo/delete/{id}", deleteTodoById).Methods("DELETE")
-	fmt.Printf("Starting server at port 8080.....\n")
+	fmt.Println("Starting server at port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
